a6cc24ff04faaa4da5bce099a7287f5c3f3e703a
laska-adief <laskaadief.la@gmail.com>
2024-01-18 18:40:27 +0700

    [Employee Organize] create function login in auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
 
+export type UserLogin = {
+  email: string;
+  password: string;
+};
+
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   constructor() {}
+
+  login(user: UserLogin) {
+    const userAuth = JSON.stringify(user);
+    localStorage.setItem('authEmployeeOrganize', userAuth);
+  }
 }

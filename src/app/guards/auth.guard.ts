c93277c53c22ff923ef0bc8a95228583fbdd5428
laska-adief <laskaadief.la@gmail.com>
2024-01-21 17:18:53 +0700

    [Employee Organize] update auth guard navigate to login page if not login

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,10 +1,12 @@
 import { inject } from '@angular/core';
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
+  const router = inject(Router);
   if (!authService.isLogin()) {
+    router.navigate(['login']);
     return false;
   }
   return true;

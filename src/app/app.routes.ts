a520ffbf7b3c062999e6faa11ba11f419637c796
laska-adief <laskaadief.la@gmail.com>
2024-01-21 17:12:36 +0700

    [Employee Organize] update routes for employee page routing integrate with auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { EmployeeComponent } from './pages/employee/employee.component';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent, title: 'Login' },
+  {
+    path: 'employee',
+    component: EmployeeComponent,
+    title: 'Employee',
+    canActivate: [authGuard],
+  },
 ];

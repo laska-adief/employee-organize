9ed436e909372e870b2d6343b5f19df0fd4bccd7
laska-adief <laskaadief.la@gmail.com>
2024-01-22 09:44:42 +0700

    [Employee Organize] add children router in employee route for list employee page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { EmployeeComponent } from './pages/employee/employee.component';
 import { authGuard } from './guards/auth.guard';
+import { EmployeeListComponent } from './pages/employee/employee-list/employee-list.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'employee', pathMatch: 'full' },
@@ -11,6 +12,13 @@ export const routes: Routes = [
     component: EmployeeComponent,
     title: 'Employee',
     canActivate: [authGuard],
+    children: [
+      {
+        path: 'list',
+        component: EmployeeListComponent,
+        title: 'Employee List',
+      },
+    ],
   },
   { path: '**', redirectTo: 'employee' },
 ];

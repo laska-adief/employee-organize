f87b8457722171d4094c354230178fce2bcbb76a
laska-adief <laskaadief.la@gmail.com>
2024-01-22 10:07:32 +0700

    [Employee Organize] import module navbar component and router outlet in employee page

--- src/app/pages/employee/employee.component.ts
+++ src/app/pages/employee/employee.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
 
 @Component({
   selector: 'app-employee',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.scss',
+  imports: [RouterOutlet, NavbarComponent],
 })
 export class EmployeeComponent {}

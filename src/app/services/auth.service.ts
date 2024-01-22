37e22c0659dfbd2c689af48fbba8933f69df5226
laska-adief <laskaadief.la@gmail.com>
2024-01-22 10:03:50 +0700

    [Employee Organize] fix localStorage is not defined

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 
 export type UserLogin = {
   email: string;
@@ -16,8 +16,23 @@ export class AuthService {
     localStorage.setItem('authEmployeeOrganize', userAuth);
   }
 
+  isLocalStorageSupported() {
+    try {
+      const testKey = '__testKey__';
+      localStorage.setItem(testKey, testKey);
+      localStorage.removeItem(testKey);
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
   isLogin() {
-    const authLogin = localStorage.getItem('authEmployeeOrganize');
-    return authLogin ? true : false;
+    if (this.isLocalStorageSupported()) {
+      const authLogin = localStorage.getItem('authEmployeeOrganize');
+      return authLogin ? true : false;
+    } else {
+      return false;
+    }
   }
 }

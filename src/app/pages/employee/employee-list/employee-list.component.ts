508288d24df5e7e70acf6d0bdaadd15ececce07a
laska-adief <laskaadief.la@gmail.com>
2024-01-24 10:41:40 +0700

    [Employee Organzie] import module for table employee list

--- src/app/pages/employee/employee-list/employee-list.component.ts
+++ src/app/pages/employee/employee-list/employee-list.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { Employee } from '../../../services/employee.service';
 
 @Component({
   selector: 'app-employee-list',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule, MatSortModule, MatPaginatorModule],
   templateUrl: './employee-list.component.html',
-  styleUrl: './employee-list.component.scss'
+  styleUrl: './employee-list.component.scss',
 })
 export class EmployeeListComponent {
-
+  displayedColumns: string[] = ['firstname', 'lastname', 'email', 'jobtitle'];
+  dataSource!: MatTableDataSource<Employee>;
 }

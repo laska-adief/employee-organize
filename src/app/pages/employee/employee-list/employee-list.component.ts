f2437e37476d6e1f6e9537e93333dda2fb051a04
laska-adief <laskaadief.la@gmail.com>
2024-01-25 07:00:14 +0700

    [Employee Organize] inject employee service in employee-list component

--- src/app/pages/employee/employee-list/employee-list.component.ts
+++ src/app/pages/employee/employee-list/employee-list.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatSort, MatSortModule } from '@angular/material/sort';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
-import { Employee } from '../../../services/employee.service';
+import { Employee, EmployeeService } from '../../../services/employee.service';
 
 @Component({
   selector: 'app-employee-list',
@@ -12,6 +12,7 @@ import { Employee } from '../../../services/employee.service';
   styleUrl: './employee-list.component.scss',
 })
 export class EmployeeListComponent {
+  employeeService = inject(EmployeeService);
   displayedColumns: string[] = ['firstname', 'lastname', 'email', 'jobtitle'];
   dataSource!: MatTableDataSource<Employee>;
 }

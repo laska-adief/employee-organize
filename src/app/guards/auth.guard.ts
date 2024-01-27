8d1ec181d12238f770b2a389852a063e44efad20
laska-adief <laskaadief.la@gmail.com>
2024-01-27 17:19:16 +0700

    [Employee Organize] add authLoginGuard to prevent access login page if already login

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -2,6 +2,17 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
+export const authLoginGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  if (authService.isLogin()) {
+    router.navigate(['employee']);
+    return false;
+  }
+  return true;
+};
+
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);

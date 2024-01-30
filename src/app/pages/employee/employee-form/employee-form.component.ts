ef042d9d4c9540e6aadba54cff8a4d95d64d81d4
laska-adief <laskaadief.la@gmail.com>
2024-01-30 13:14:34 +0700

    [Employee Organize] import module MatDatePickerModule

--- src/app/pages/employee/employee-form/employee-form.component.ts
+++ src/app/pages/employee/employee-form/employee-form.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 @Component({
   selector: 'app-employee-form',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatSelectModule],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDatepickerModule,
+  ],
   templateUrl: './employee-form.component.html',
   styleUrl: './employee-form.component.scss',
 })

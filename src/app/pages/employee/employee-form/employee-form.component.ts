1b34bb1a0d4fdf373c8e8558cf2e43ea651d3bc6
laska-adief <laskaadief.la@gmail.com>
2024-01-30 13:14:03 +0700

    [Employee Organize] import module MatSelectModule

--- src/app/pages/employee/employee-form/employee-form.component.ts
+++ src/app/pages/employee/employee-form/employee-form.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 @Component({
   selector: 'app-employee-form',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule],
+  imports: [MatFormFieldModule, MatInputModule, MatSelectModule],
   templateUrl: './employee-form.component.html',
   styleUrl: './employee-form.component.scss',
 })

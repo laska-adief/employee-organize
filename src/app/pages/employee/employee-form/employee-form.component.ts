acea8f478648bd9f675775c8c34bd6419d6d09e6
laska-adief <laskaadief.la@gmail.com>
2024-02-14 13:54:14 +0700

    [Employee Organize] inject FormBuilder and create FormGroup

--- src/app/pages/employee/employee-form/employee-form.component.ts
+++ src/app/pages/employee/employee-form/employee-form.component.ts
@@ -5,6 +5,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatRadioModule } from '@angular/material/radio';
+import { FormBuilder, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-employee-form',
   standalone: true,
@@ -19,4 +20,7 @@ import { MatRadioModule } from '@angular/material/radio';
   templateUrl: './employee-form.component.html',
   styleUrl: './employee-form.component.scss',
 })
-export class EmployeeFormComponent {}
+export class EmployeeFormComponent {
+  formEmployee!: FormGroup;
+  constructor(private fb: FormBuilder) {}
+}
